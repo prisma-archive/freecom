a91afc78e28bcf385816f4a535d0741e1ca8539e
Nikolas Burk <nikolas.burk@gmail.com>
2017-03-10 17:26:22 +0100

    more refactoring

--- frontend/src/components/ChatMessage.js
+++ frontend/src/components/ChatMessage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './ChatMessage.css'
-import { timeDifference } from '../utils'
+import { timeDifferenceForDate } from '../utils'
 
 class ChatMessage extends Component {
 
@@ -9,25 +9,36 @@ class ChatMessage extends Component {
   }
 
   render() {
+  const {agent} = this._generateChatMessageInfo()
+  return (
+      <div className='fadeInLeft'>
+        {agent ? this._renderAgentMessage() : this._renderOwnMessage()}
+      </div>
+    )
+  }
 
-    const createdAtTimestamp = new Date(this.props.message.createdAt).getTime()
-    const nowTimestamp = new Date().getTime()
-    const ago = timeDifference(nowTimestamp, createdAtTimestamp)
-
-    const agent = this.props.message.agent
-    const profileImageUrl = agent && agent.imageUrl ? agent.imageUrl : global['Freecom'].companyLogoURL
-
-    const textWithLinebreaks = this.props.message.text.split('\n').map((line, index) => (
-      <span key={index}>
-        {line}
-        <br />
-      </span>
-    ))
-
+  _renderOwnMessage = () => {
+    const {ago, textWithLineBreaks} = this._generateChatMessageInfo()
     return (
-    <div className='fadeInLeft'>
+      <div className='message-padding'>
+        <div className='flex flex-bottom'>
+          <div className='message-container message-container-padding-right flex-right'>
+            <div
+              style={{backgroundColor: global['Freecom'].mainColor}}
+              className='white padding-20 radius background-blue'>
+              <p>{textWithLineBreaks}</p>
+            </div>
+            <p className='right opacity-4 padding-top-2'>{ago}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-      <div style={{display: this.props.message.agent ? 'visible' : 'none'}} className='message-padding'>
+  _renderAgentMessage = () => {
+    const {ago, profileImageUrl, textWithLineBreaks} = this._generateChatMessageInfo()
+    return (
+      <div className='message-padding'>
         <div className='flex flex-bottom'>
           <img
             src={profileImageUrl}
@@ -35,29 +46,26 @@ class ChatMessage extends Component {
             className='avatar message-avatar'></img>
           <div className='message-container message-container-padding-left'>
             <div className='opaque background-gray padding-20 radius opaque'>
-              <p>{this.props.message.text}</p>
+              <p>{textWithLineBreaks}</p>
             </div>
             <p className='right opacity-4 padding-top-2'>{ago}</p>
           </div>
         </div>
       </div>
-
-      <div style={{display: !this.props.message.agent ? 'visible' : 'none'}} className='message-padding'>
-        <div className='flex flex-bottom'>
-          <div className='message-container message-container-padding-right flex-right'>
-            <div
-              style={{backgroundColor: global['Freecom'].mainColor}}
-              className='white padding-20 radius background-blue'>
-              <p>{textWithLinebreaks}</p>
-            </div>
-            <p className='right opacity-4 padding-top-2'>{ago}</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
     )
+  }
 
+  _generateChatMessageInfo = () => {
+    const ago = timeDifferenceForDate(this.props.message.createdAt)
+    const agent = this.props.message.agent
+    const profileImageUrl = agent && agent.imageUrl ? agent.imageUrl : global['Freecom'].companyLogoURL
+    const textWithLineBreaks = this.props.message.text.split('\n').map((line, index) => (
+      <span key={index}>
+        {line}
+        <br />
+      </span>
+    ))
+    return {ago, agent, profileImageUrl, textWithLineBreaks}
   }
 
 }
